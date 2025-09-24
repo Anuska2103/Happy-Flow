7cdbafff829775c242453fa5247fb506d8664558
Anuska2103 <anuskabasak2110@gmail.com>
2025-09-24 23:10:28 +0530

    config changes

--- src/firebase/config.js
+++ src/firebase/config.js
@@ -1,16 +1,15 @@
-import { initializeApp, getApps } from 'firebase/app';
+import { initializeApp, getApps, getApp } from 'firebase/app';
 import { getAuth } from 'firebase/auth';
 import { getFirestore } from "firebase/firestore";
 
-
-// IMPORTANT: Replace with your Firebase project's configuration
+// The keys for the public facing Firebase JS SDK
 const firebaseConfig = {
-  apiKey: "AIzaSyChBFS1WUBSwcCKkjuKeZ4mKbA9VbIYmYM",
-  authDomain: "first-project-e8c14.firebaseapp.com",
-  projectId: "first-project-e8c14",
-  storageBucket: "first-project-e8c14.firebasestorage.app",
-  messagingSenderId: "807011360991",
-  appId: "1:807011360991:web:8f525c1f1bcd05689c496a" 
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
